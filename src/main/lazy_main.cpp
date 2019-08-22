28a2388c2778172c1bb82e8bd9c879588ccefb30
nishimoto <a32b16c4@gmail.com>
2019-08-23 01:30:29 +0900

    minor change

--- src/main/lazy_main.cpp
+++ src/main/lazy_main.cpp
@@ -156,7 +156,7 @@ int main(int argc, char *argv[])
     }
     else
     {
-        stool::write_vector(outputFile, attrs);
+        stool::write_vector(outputFile, attrs, true);
 
         //IO::write(outputFile, attrs, UINT64_MAX - 1);
     }
